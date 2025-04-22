e2d6d27624db25c1615fcb32884bd47790cf51f1
rushikeshg25 <rushikeshvghotekar25@gmail.com>
2025-04-23 02:15:07 +0530

    init: anchor boilerplate

---
+++ programs/amm/src/lib.rs
@@ -0,0 +1,16 @@
+use anchor_lang::prelude::*;
+
+declare_id!("2tD2D3Ld2nt4CCjQNfDB8S5N2iJBjyj3FWD4cMCVaidF");
+
+#[program]
+pub mod amm {
+    use super::*;
+
+    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+        msg!("Greetings from: {:?}", ctx.program_id);
+        Ok(())
+    }
+}
+
+#[derive(Accounts)]
+pub struct Initialize {}
